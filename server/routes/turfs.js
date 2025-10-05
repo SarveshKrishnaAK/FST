c545de9bdafb9f381b416f31ecc9b0452321cfce
sarveshkrishnaak <sarveshkrishnaak@gmail.com>
2025-10-05 15:14:07 +0530

    Changes in models

--- server/routes/turfs.js
+++ server/routes/turfs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Turf = require('./models/Turf');
+const Turf = require('../models/Turf');
 
 // Get all turfs
 router.get('/', async (req, res) => {
